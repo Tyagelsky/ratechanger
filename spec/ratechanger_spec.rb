b85399a73f57b5f8d02c751e0dd1337a76c30327
Yaroslav <tyagelsky@yandex.ru>
2017-09-05 19:58:23 +0300

    add some rspec test for public methods

--- spec/ratechanger_spec.rb
+++ spec/ratechanger_spec.rb
@@ -5,7 +5,17 @@ RSpec.describe Ratechanger do
     expect(Ratechanger::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "must calculate rating for winner" do
+    result = Ratechanger::Core.new(winner: 2000, looser: 1500, koef: 10)
+    expect(result.calculate_rating_for_winner).not_to be nil
+    expect(result.calculate_rating_for_winner).not_to eq(2000)
+    expect(result.calculate_rating_for_winner).to be > 2000
+  end
+
+  it "must calculate rating for looser" do
+    result = Ratechanger::Core.new(winner: 2000, looser: 1500, koef: 10)
+    expect(result.calculate_rating_for_looser).not_to be nil
+    expect(result.calculate_rating_for_looser).not_to eq(1500)
+    expect(result.calculate_rating_for_looser).to be < 1500
   end
 end
